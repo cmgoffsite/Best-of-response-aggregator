2161fc5dd075968bb44cde74b98e3132044b3f27
cmgoffsite <155569644+cmgoffsite@users.noreply.github.com>
2024-03-07 11:12:05 -0600

    Update Aggregate.py

--- Aggregate.py
+++ Aggregate.py
@@ -14,9 +14,8 @@ def convert_to_csv_url(sheet_url):
 
 def to_excel(df):
     output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='openpyxl')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
-    writer.save()
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
     processed_data = output.getvalue()
     return processed_data
 
